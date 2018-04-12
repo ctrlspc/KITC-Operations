0bc8c990945690e48eee6a340def22014dd7e79a
Jason <ctrlspc@gmail.com>
2018-04-12 17:16:00 +0100

    Replaced Header title with KITC logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export const Header = ({ startLogout }) => (
     <div className="content-container">
       <div className="header__content">
         <Link className="header__title" to="/dashboard">
-          <h1>Project Operations Tracker</h1>
+          <img src="/images/kitc-logo.png" alt="The KITC Logo"/>
         </Link>
         <button className="button button--link" onClick={startLogout}>Logout</button>
       </div>
